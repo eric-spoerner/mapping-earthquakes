3e9425fb81674275b69f6c23cf5ef9db76c638ad
Eric Spoerner <espoerner@gmail.com>
2022-02-18 09:56:10 -0800

    Added neighborhood geojson and first updates to street map.

--- mapping_geojson_polygons/static/js/logic.js
+++ mapping_geojson_polygons/static/js/logic.js
@@ -1,12 +1,12 @@
 // // We create the tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // // Second tile layer -- dark streets.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -15,14 +15,14 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // create base layer that holds both map layers
 // key will be label name in UX
 let baseMaps = {
-  Day: light,
-  Night: dark
+  Streets: streets,
+  'Satllite Streets': satelliteStreets
 };
 
 let map = L.map('mapid', {
-  center: [44.0, -80.0],  //center on toronto
-  zoom: 2,
-  layers: [dark]
+  center: [43.7, -79.3],  //center on toronto
+  zoom: 11,
+  layers: [satelliteStreets]
 });
 
 L.control.layers(baseMaps).addTo(map);
